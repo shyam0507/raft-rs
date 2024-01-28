1cecafb146c339e0d837a3a59e4b7fa5d130f3c7
shyam0507 <shyam432168@gmail.com>
2024-01-28 11:57:31 +0530

    minor enhancements to leader election

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::{
     env::{self, args},
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
-    process::id,
-    str::Utf8Error,
     sync::{Arc, Mutex},
     thread::{self, sleep},
-    time::{self, Duration, Instant},
+    time::{Duration, Instant},
 };
 
 #[derive(Debug, PartialEq)]
@@ -35,6 +33,7 @@ struct RequestVotePayload {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct VoteResponse {
+    server_id: u32,
     term: u64,
     vote_granted: bool,
 }
@@ -53,17 +52,18 @@ struct Server {
     election_timeout: Duration,
     port: u32,
     peer_addr: Vec<String>,
+    votes_response: Vec<VoteResponse>,
 }
 
 impl Server {
-    fn init(id: u32, host: String, port: u32, peers: Vec<String>) -> Self {
+    fn init(id: u32, host: String, port: u32, peers: &Vec<String>) -> Self {
         let election_timeout = Duration::from_millis(random!(1000..=3500));
         let server_addr = format!("127.0.0.1:{}", port);
         let mut peer_addr: Vec<String> = vec![];
 
         for peer in peers {
-            if peer != server_addr {
-                peer_addr.push(peer)
+            if peer != &server_addr {
+                peer_addr.push(peer.to_string())
             }
         }
 
@@ -80,10 +80,10 @@ impl Server {
             election_timeout,
             port,
             peer_addr,
+            votes_response: vec![],
         }
     }
 
-    fn start(&self) {}
     fn append_entry(&self, entries: Vec<Log>) {}
     fn request_vote(&mut self) {
         self.state = ServerState::Candidate;
@@ -102,6 +102,7 @@ impl Server {
         let string_json = serde_json::to_string(&request_payload).unwrap() + "\n";
         //request vote from peers
         for peer in &self.peer_addr {
+            //TODO parallelize this
             let mut stream = TcpStream::connect(peer).unwrap();
             let _ = stream.write_all(&string_json.as_bytes());
 
@@ -117,10 +118,7 @@ impl Server {
                 }
             };
 
-            if vote_res.vote_granted {
-                self.state = ServerState::Leader;
-                println!("Leader Elected")
-            }
+            self.votes_response.push(vote_res)
         }
     }
     fn handle_request_vote_request(&mut self, vote_request: RequestVotePayload) -> VoteResponse {
@@ -131,21 +129,27 @@ impl Server {
             self.voted_for = Some(vote_request.candidate_id);
             self.current_term = vote_request.term;
 
-            println!("Voted from term {}", vote_request.term);
+            println!(
+                "Voted for term {} for server {}",
+                vote_request.term, vote_request.candidate_id
+            );
 
             return VoteResponse {
                 term: vote_request.term,
                 vote_granted: true,
+                server_id: self.id,
             };
         } else {
             return VoteResponse {
                 term: self.current_term,
                 vote_granted: false,
+                server_id: self.id,
             };
         }
     }
     fn become_leader(&mut self) {
         self.state = ServerState::Leader;
+        println!("Server {} became leader", self.id);
     }
     fn election_timeout_elapsed(&self) -> bool {
         self.leader_last_hearbeat.elapsed() > self.election_timeout
@@ -161,14 +165,14 @@ fn main() {
     let peers = vec![
         "127.0.0.1:3000".to_string(),
         "127.0.0.1:3001".to_string(),
-        // "127.0.0.1:3002".to_string(),
+        "127.0.0.1:3002".to_string(),
     ];
 
     let server = Arc::new(Mutex::new(Server::init(
         port,
         "127.0.0.1".to_string(),
         port,
-        peers,
+        &peers,
     )));
     println!("Server config is {:#?}", server);
     let server_clone = Arc::clone(&server);
@@ -183,8 +187,24 @@ fn main() {
             && gaurd.election_timeout_elapsed()
             && gaurd.voted_for == None
         {
-            println!("heartbeat timedout going to election");
+            println!("heartbeat timedout going for election");
             gaurd.request_vote();
+            // drop(gaurd)
+        } else if gaurd.state == ServerState::Candidate {
+            //check if enough votes received or election timedout
+            let mut votes_rec = 0;
+            let vote_response = &gaurd.votes_response;
+            for vote in vote_response {
+                if vote.vote_granted {
+                    votes_rec += 1;
+                }
+            }
+
+            if votes_rec >= peers.len() / 2 {
+                gaurd.become_leader();
+            } else {
+                //TODO implement split vote
+            }
         } else {
             drop(gaurd);
         }
